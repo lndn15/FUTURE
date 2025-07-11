3796202e4e7764a0450cbb9cfdf62992a81e86ff
lndn15 <lndn15evro@gmail.com>
2025-07-12 00:28:12 +0400

    Update core.py

--- core.py
+++ core.py
@@ -1,8 +1,36 @@
-from core.command import BaseCommand
-from abc import ABC, abstractmethod
+from .history import CommandHistory
+from modules.searcher import SearchCommand
+from modules.system import SystemCommand
 
-
-class Assistant:
+class Core:
     def __init__(self):
-        self.commands: dict[str, type[BaseCommand]] = {}
-        self.history: list[str] = []
+        self._comnd = {
+            "поиск": SearchCommand,
+            "помощь": self.showhelp,
+            "система": SystemCommand,
+            "история": self.historyshow 
+        }
+        self._hist = CommandHistory()
+
+    def historyshow(self) -> str:
+        return "\n".join(self._hist[-10:]) if self._hist else "История пуста"
+    def showhelp(self) -> str:
+        return "В ядре доступны команды: search; system; history"
+    def worka(self, user_input: str) -> str:
+        self._hist.add(user_input)
+
+        if not user_input.strip():
+            return "Неизвестная команда"
+        
+        parts = user_input.split()
+        mainc, args = parts[0], parts[1:]
+
+        if mainc not in self._comnd:
+            return "Неизвестная команда; Введите help для массива команд"
+        try:
+            return str(self._comnd[mainc](*args))
+        except KeyError:
+            return "Неизвестная команда"
+        except Exception as e:
+            return f"Ошибка: {str(e)}"    
+        
