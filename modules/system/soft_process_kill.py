2d03e54e5f2a0e27980573e514e8ae691e309723
lndn15 <lndn15evro@gmail.com>
2025-07-15 09:36:47 +0400

    Update soft_process_kill.py

--- modules/system/soft_process_kill.py
+++ modules/system/soft_process_kill.py
@@ -1,1 +1,19 @@
+import psutil
 
+class SoftProcessKiller:
+    
+    def kill_process(identifier):
+
+        try:
+            if isinstance(identifier, int) or str(identifier).isdigit():
+                psutil.Process(int(identifier)).terminate()
+                return True
+            
+            for proc in psutil.process_iter(['pid', 'name']):
+                if str(identifier).lower() in proc.info['name'].lower():
+                    proc.terminate()
+                    return True
+            return False
+        
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            return False
