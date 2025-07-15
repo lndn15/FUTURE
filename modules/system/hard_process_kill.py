1c6820b721221af6ed794c71d17056d428c0d8b8
lndn15 <lndn15evro@gmail.com>
2025-07-15 09:37:51 +0400

    Update hard_process_kill.py

--- modules/system/hard_process_kill.py
+++ modules/system/hard_process_kill.py
@@ -1,1 +1,19 @@
+import psutil
 
+class HardProcessKiller:
+
+    def kill_process(identifier):
+
+        try:
+            if isinstance(identifier, int) or str(identifier).isdigit():
+                psutil.Process(int(identifier)).kill()
+                return True
+            
+            for proc in psutil.process_iter(['pid', 'name']):
+                if str(identifier).lower() in proc.info['name'].lower():
+                    proc.kill()
+                    return True
+            return False
+        
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            return False
