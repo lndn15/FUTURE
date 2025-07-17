8bf46e8d9536900520421d42c967fa75da8002f7
lndn15 <lndn15evro@gmail.com>
2025-07-17 08:53:39 +0400

    Update core.py

--- core/core.py
+++ core/core.py
@@ -1,36 +1,8 @@
-from .history import CommandHistory
-from modules.searcher import SearchCommand
-from modules.system import SystemCommand
-
-class Core:
+class Komandir:
     def __init__(self):
-        self._comnd = {
-            "поиск": SearchCommand,
-            "помощь": self.showhelp,
-            "система": SystemCommand,
-            "история": self.historyshow 
-        }
-        self._hist = CommandHistory()
+        self._comd = {}
+        self._hist = []
 
-    def historyshow(self) -> str:
-        return "\n".join(self._hist[-10:]) if self._hist else "История пуста"
-    def showhelp(self) -> str:
-        return "В ядре доступны команды: search; system; history"
-    def worka(self, user_input: str) -> str:
-        self._hist.add(user_input)
-
-        if not user_input.strip():
-            return "Неизвестная команда"
-        
-        parts = user_input.split()
-        mainc, args = parts[0], parts[1:]
-
-        if mainc not in self._comnd:
-            return "Неизвестная команда; Введите help для массива команд"
-        try:
-            return str(self._comnd[mainc](*args))
-        except KeyError:
-            return "Неизвестная команда"
-        except Exception as e:
-            return f"Ошибка: {str(e)}"    
+    def register(self, name: str, handler: callable):
+        self._comnd[name] = {"handler": handler} 
         
