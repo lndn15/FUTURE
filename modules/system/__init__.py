1c9f85f647d432de188bcb8e64b0615bb7cef59b
lndn15 <lndn15evro@gmail.com>
2025-07-17 08:51:17 +0400

    \

--- modules/system/__init__.py
+++ modules/system/__init__.py
@@ -1,1 +1,6 @@
+from .hard_process_kill import HardProcessKiller
+from .soft_process_kill import SoftProcessKiller
+from .system_monitor import SystemMonitor
+from .process_manager import ProcessManager
 
+all = ["HardProcessKiller", "SoftProcessKiller", "SystemMonitor", "ProcessManager", "SystemCommand"]
